9bf6d45584df7d9419edfc82ae69d854df4e4eca
Oksana Artemeva <tek1122@mail.ru>
2020-12-06 20:08:10 +0300

    дз 3(исправленное)

--- Lesson_3/src/main/java/Notebook.java
+++ Lesson_3/src/main/java/Notebook.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Notebook {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Notebook {
+    HashMap<String, HashSet<String>> phoneBook;
+
+    public Notebook() {
+        this.phoneBook = new HashMap<>();
+    }
+
+    public void addContact(String surname, String phone) {
+        HashSet<String> book = phoneBook.getOrDefault(surname, new HashSet<>());
+        book.add(phone);
+        phoneBook.put(surname, book);
+    }
+
+    public void print(String surname){
+        System.out.println("Фамилия: "+surname+". Номер телефона: "+phoneBook.getOrDefault(surname,new HashSet<>()));
+    }
 }
